040fa2ed8867e239f56ce98fcba9e6c125c5bb85
hanggara <hanggaraputra292@gmail.com>
2024-06-14 12:46:41 +0700

    uiTrx

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:moneytracker/pages/category_page.dart';
 import 'package:moneytracker/pages/home_page.dart';
+import 'package:moneytracker/pages/transaction_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -24,21 +25,40 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: (currentIndex == 0) ? CalendarAppBar(
-        accent: Colors.green,
-        backButton: false,
-        locale: 'id',
-        onDateChanged: (value) => print(value),
-        firstDate: DateTime.now().subtract(Duration(days: 140)),
-        lastDate: DateTime.now(),
-      ) : PreferredSize(preferredSize: Size.fromHeight(100), child: Container(child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
-        child: Text('Categories', style: GoogleFonts.montserrat(fontSize: 20),),
-      ))),
+      appBar: (currentIndex == 0)
+          ? CalendarAppBar(
+              accent: Colors.green,
+              backButton: false,
+              locale: 'id',
+              onDateChanged: (value) => print(value),
+              firstDate: DateTime.now().subtract(Duration(days: 140)),
+              lastDate: DateTime.now(),
+            )
+          : PreferredSize(
+              preferredSize: Size.fromHeight(100),
+              child: Container(
+                  child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+                child: Text(
+                  'Categories',
+                  style: GoogleFonts.montserrat(fontSize: 20),
+                ),
+              ))),
       floatingActionButton: Visibility(
         visible: (currentIndex == 0) ? true : false,
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context)
+                .push(
+              MaterialPageRoute(
+                builder: (context) => TransactionPage(),
+              ),
+            )
+                .then((value) {
+              setState(() {});
+            });
+          },
           backgroundColor: Colors.green,
           child: Icon(Icons.add),
           shape: CircleBorder(),
